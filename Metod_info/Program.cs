e9e24a8902c542dfb84498f250280decd77fd440
Polina <polinka29enp@gmail.com>
2022-09-03 07:32:52 +0300

    Двумерный массив и вывод его дублем

---
+++ Metod_info/Program.cs
@@ -0,0 +1,29 @@
+﻿// Всегда можно использовать для создания двухмерного метода с рандомными числами этот код:
+int m = new Random().Next(1, 5);
+int n = new Random().Next(1, 5);
+int[,] myarray = new int[m, n];
+for (int i = 0; i < myarray.GetLength(0); i++)
+{
+    for (int j = 0; j < myarray.GetLength(1); j++)
+    {
+        myarray[i, j] = new Random().Next(1, 10);
+        Console.Write($"{myarray[i , j]} ");
+    }
+    Console.WriteLine();
+}
+
+// Для вывода на печать еще раз типо дубля этот метод:
+void PrintArray(int[,] array)
+{
+for(int i = 0;i < m;i++)
+    {
+    for(int j = 0;j < n;j++)
+      {
+       Console.Write(array[i, j] + " ");
+      }
+    Console.WriteLine();
+    }
+    return;
+}
+Console.WriteLine();
+PrintArray(myarray);
